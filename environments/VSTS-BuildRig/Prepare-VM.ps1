720225181ef4963b8b19680aed2655728c2477b0
Markus Heiliger <markus.heiliger@dash-two.de>
2018-01-30 20:42:51 -0800

    removed 2nd CSE and introducted prepare-vm script

---
+++ environments/VSTS-BuildRig/Prepare-VM.ps1
@@ -0,0 +1,14 @@
+#Requires -Version 3.0
+
+[CmdletBinding()]
+Param(
+    [string] [Parameter(Mandatory=$true)]    $VSTSAccount,
+    [string] [Parameter(Mandatory=$true)]    $PersonalAccessToken,
+    [string] [Parameter(Mandatory=$true)]    $PoolName,
+    [string] [Parameter(Mandatory=$false)]   $AgentName = $($env:COMPUTERNAME),
+    [string] [Parameter(Mandatory=$false)]   $ChocoPackages = ""
+)
+
+.\Install-WindowsAgent.ps1 -vstsAccount $VSTSAccount -personalAccessToken $PersonalAccessToken -PoolName $PoolName -AgentName $AgentName
+
+if ($ChocoPackages) { .\Install-Chocolatey.ps1 -Packages $ChocoPackages }
