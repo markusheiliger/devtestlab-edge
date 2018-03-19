bbe1a61f58e119a89267fb2ac56046bc7fc203fe
Markus Heiliger <markus.heiliger@dash-two.de>
2018-03-19 14:11:13 -0700

    create kubectl config in cluster owner home directory

--- environments/AKS/azuredeploy.operations.sh
+++ environments/AKS/azuredeploy.operations.sh
@@ -5,17 +5,29 @@
 # $3 = Tenant Id
 # $4 = Cluster Name
 # $5 = Cluster RG Name
+# $6 = Cluster Owner
+
+export LOG=/var/lib/waagent/custom-script/download/0/azuredeploy.operations.log
+
+echo "Provisioning as $(whoami)" >> $LOG
 
 AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
      sudo tee /etc/apt/sources.list.d/azure-cli.list
 
+echo "Registering Azure package repo and installing Azure CLI" >> $LOG
 sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
 sudo apt-get install apt-transport-https
 sudo apt-get update && sudo apt-get install azure-cli
 
+echo "Using service principal $1 to login Azure CLI ..." >> $LOG
 sudo az login --service-principal -u $1 -p $2 -t $3
 
+echo "Installing KubeCtl using Azure CLI ..." >> $LOG
 sudo az aks install-cli
-sudo az aks get-credentials --resource-group $5 --name $4
 
+echo "Ensuring user home for $6 ..." >> $LOG
+sudo mkhomedir_helper $6
+
+echo "Getting credentials for KubeCtl ..." >> $LOG
+sudo az aks get-credentials --resource-group $5 --name $4 --file /home/$6/.kube/config
