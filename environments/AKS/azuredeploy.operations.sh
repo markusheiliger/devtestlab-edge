e3b63f137d94dbc5c8e999c7fa9b0ee0b4f5fcdf
Markus Heiliger <markus.heiliger@dash-two.de>
2018-03-20 15:34:14 -0700

    removed custom log

--- environments/AKS/azuredeploy.operations.sh
+++ environments/AKS/azuredeploy.operations.sh
@@ -7,15 +7,13 @@
 # $5 = Cluster RG Name
 # $6 = Cluster Owner
 
-export LOG=/var/lib/waagent/custom-script/download/0/azuredeploy.operations.log
-
-echo "Provisioning as $(whoami)" >> $LOG
+echo "### Provisioning as $(whoami)"
 
 AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
      sudo tee /etc/apt/sources.list.d/azure-cli.list
 
-echo "Registering Azure package repo and installing Azure CLI" >> $LOG
+echo "### Registering Azure package repo and installing Azure CLI"
 sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
 
 sudo apt-get install -y apt-transport-https
@@ -24,22 +22,22 @@ sudo apt-get update -y
 sudo apt-get install -y azure-cli
 sudo apt-get update -y
 
-echo "Using service principal $1 to login Azure CLI ..." >> $LOG
+echo "### Using service principal $1 to login Azure CLI ..."
 sudo az login --service-principal -u $1 -p $2 -t $3 
 
-echo "Installing kubectl using Azure CLI ..." >> $LOG
+echo "### Installing kubectl using Azure CLI ..."
 sudo az aks install-cli 
 
-echo "Getting credentials for kubectl ..." >> $LOG
+echo "### Getting credentials for kubectl ..."
 sudo az aks get-credentials -g $5 -n $4 -a 
 
-echo "Installing helm ..." >> $LOG
+echo "### Installing helm ..."
 sudo curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
 
-echo "Initializing helm" >> $LOG
+echo "### Initializing helm"
 sudo helm init
 
-echo "Creating startup script to prepare kubectl config ..." >> $LOG
+echo "### Creating startup script to prepare kubectl config ..."
 sudo tee -a /etc/profile.d/copy-kubectl-config.sh << END
 ME="\$(whoami)"
 if [ ! -d "~/.kube" ]; then
@@ -48,5 +46,5 @@ if [ ! -d "~/.kube" ]; then
 fi
 END
 
-echo "Enable execution on startup script ..." >> $LOG
+echo "### Enable execution on startup script ..."
 sudo chmod +x /etc/profile.d/copy-kubectl-config.sh
