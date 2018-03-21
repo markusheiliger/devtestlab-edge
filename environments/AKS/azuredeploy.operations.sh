43c63d6cf4dd632b44585b5df5b9e0dc1402c9ca
Markus Heiliger <markus.heiliger@dash-two.de>
2018-03-21 12:55:45 -0700

    adde debian frontend config

--- environments/AKS/azuredeploy.operations.sh
+++ environments/AKS/azuredeploy.operations.sh
@@ -9,6 +9,8 @@
 
 echo "### Provisioning as $(whoami)" >&2
 
+export DEBIAN_FRONTEND=noninteractive
+
 AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
      sudo tee /etc/apt/sources.list.d/azure-cli.list
