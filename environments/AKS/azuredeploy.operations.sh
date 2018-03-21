31e48122cda25ec2f88a7695a79bd9d615c77d36
Markus Heiliger <markus.heiliger@dash-two.de>
2018-03-21 11:28:25 -0700

    fixed some permission issues

--- environments/AKS/azuredeploy.operations.sh
+++ environments/AKS/azuredeploy.operations.sh
@@ -32,7 +32,10 @@ echo "### Getting credentials for kubectl ..."
 sudo az aks get-credentials -g $5 -n $4 -a 
 
 echo "### Installing helm ..."
-sudo curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
+sudo rm -f azuredeploy.operations.helm.sh
+sudo curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > azuredeploy.operations.helm.sh
+sudo chmod 700 azuredeploy.operations.helm.sh
+sudo sh azuredeploy.operations.helm.sh
 
 echo "### Initializing helm ..."
 sudo helm init
@@ -49,6 +52,7 @@ END
 echo "### Enable execution on startup script ..."
 sudo chmod +x /etc/profile.d/copy-kubectl-config.sh
 
+echo "### Fetching information to peer networks ..."
 CLUSTERLOCATION=$(az resource show -g $5 -n $4 --resource-type "Microsoft.ContainerService/ManagedClusters" --query location --output tsv)
 CLUSTERRGNAME="MC_$5_$4_$CLUSTERLOCATION"
 CLUSTERVNETID=$(az resource list -g $CLUSTERRGNAME --resource-type "Microsoft.Network/virtualNetworks" --query '[0].id' --output tsv)
@@ -56,5 +60,6 @@ CLUSTERVNETNAME=$(az resource list -g $CLUSTERRGNAME --resource-type "Microsoft.
 OPERATIONSVNETID=$(az resource list -g $5 --resource-type "Microsoft.Network/virtualNetworks" --query '[0].id' --output tsv)
 OPERATIONSVNETNAME=$(az resource list -g $5 --resource-type "Microsoft.Network/virtualNetworks" --query '[0].name' --output tsv)
 
+echo "### Peer cluster and operations vnet ..."
 az network vnet peering create --name LinkClusterToOperations --resource-group $CLUSTERRGNAME --vnet-name $CLUSTERVNETNAME --remote-vnet-id $OPERATIONSVNETID
 az network vnet peering create --name LinkOperationsToCluster --resource-group $5 --vnet-name $OPERATIONSVNETNAME --remote-vnet-id $CLUSTERVNETID --allow-vnet-access
