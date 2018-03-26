c0ce3c41126e882af66cab3a051a953f76d43ef1
Markus Heiliger <markus.heiliger@dash-two.de>
2018-03-26 07:56:21 -0700

    added automation and open port 8081 for testing

--- environments/Artifactory/azuredeploy.sh
+++ environments/Artifactory/azuredeploy.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
 
+sudo yum install -y java-1.8.0-openjdk
+
+wget https://bintray.com/jfrog/artifactory-rpms/rpm -O bintray-jfrog-artifactory-rpms.repo
+sudo mv bintray-jfrog-artifactory-rpms.repo /etc/yum.repos.d/
+sudo yum install -y jfrog-artifactory-oss
+
+sudo mkdir /opt/app
+sudo ln -s /etc/opt/jfrog/artifactory/ /opt/app/artifactory
+
+sudo service artifactory start
+
